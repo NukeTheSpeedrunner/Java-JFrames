c3b4858ac07bf550abe11e10a0d5b980427838b9
Nuke <118713529+NukeTheSpeedrunner@users.noreply.github.com>
2023-01-20 23:01:54 -0500

    Create MakeJFrame.java

---
+++ MakeJFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+class MakeJFrame {
+  
+  Jframe frame = new JFrame("title"); // set title here
+  frame.setVisible(true);
+  frame.setSize(500,300) // set size of frame
+  frame.setBackground(Color. Black); // set background color
+  
+}
